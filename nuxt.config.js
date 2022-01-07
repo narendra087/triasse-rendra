a29549ade474184550f2e5c1ef3800c7056ea7c4
Narendra <narendra.negara@homy.id>
2022-01-07 19:33:01 +0700

    Assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'triasse-rendra',
+    title: 'Triasse',
     htmlAttrs: {
       lang: 'en'
     },
